daf899ca5905586deb02ae4a7b6aa5adf48e5df8
Dale Schumacher <dalnefre@yahoo.com>
2020-06-03 11:20:55 -0600

    implement "become" (called `update` because `become` is reserved)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
 use std::collections::VecDeque;
 use std::rc::Rc;
+use std::cell::RefCell;
 
 pub trait Behavior {
     fn react(&self, event: Event) -> Effect;
 }
 
 pub struct Actor {
-    behavior: Box<dyn Behavior>,
+    behavior: RefCell<Box<dyn Behavior>>,
 }
 impl Actor {
     pub fn new(behavior: Box<dyn Behavior>) -> Rc<Actor> {
-        Rc::new(Actor { behavior })
+        Rc::new(Actor {
+            behavior: RefCell::new(behavior),
+        })
     }
     pub fn dispatch(&self, event: Event) -> Effect {
-        self.behavior.react(event)
+        self.behavior.borrow().react(event)
+    }
+    pub fn update(&self, behavior: Box<dyn Behavior>) {
+        *self.behavior.borrow_mut() = behavior;
     }
 }
 
@@ -40,6 +46,7 @@ pub enum Message {
 pub struct Effect {
     actors: Vec<Rc<Actor>>,
     events: VecDeque<Event>,
+    state: Option<Box<dyn Behavior>>,
     error: Option<&'static str>,
 }
 impl Effect {
@@ -47,6 +54,7 @@ impl Effect {
         Effect {
             actors: Vec::new(),
             events: VecDeque::new(),
+            state: None,
             error: None,
         }
     }
@@ -54,8 +62,8 @@ impl Effect {
         let event = Event::new(target, message);
         self.events.push_back(event);
     }
-    pub fn update(&mut self) {
-        //panic!("behavior replacement not implemented");
+    pub fn update(&mut self, behavior: Box<dyn Behavior>) {
+        self.state = Some(behavior);
     }
     pub fn actor_count(&self) -> usize {
         self.actors.len()
@@ -82,6 +90,7 @@ mod tests {
     #[test]
     fn sink_behavior() {
         let sink = Actor::new(Box::new(Sink {}));
+
         let event = Event::new(&sink, Message::Empty);
         let effect = sink.dispatch(event);
 
@@ -97,7 +106,7 @@ mod tests {
         fn react(&self, event: Event) -> Effect {
             let mut effect = Effect::new();
             effect.send(&self.cust, event.message);
-            effect.update();
+            effect.update(Box::new(Sink {}));
             effect
         }
     }
@@ -108,11 +117,23 @@ mod tests {
         let once = Actor::new(Box::new(Once {
             cust: Rc::clone(&sink)
         }));
+
         let event = Event::new(&once, Message::Empty);
         let effect = once.dispatch(event);
 
         assert_eq!(0, effect.actor_count());
         assert_eq!(1, effect.event_count());
+        assert_eq!(None, effect.error());
+
+        if let Some(behavior) = effect.state {
+            once.update(behavior);
+        }
+
+        let event = Event::new(&once, Message::Empty);
+        let effect = once.dispatch(event);
+
+        assert_eq!(0, effect.actor_count());
+        assert_eq!(0, effect.event_count());
         assert_eq!(None, effect.error());
     }
 }
