3000d4ff6ee719ee38b3136e2839b810ab8452c0
Dale Schumacher <dalnefre@yahoo.com>
2020-06-06 12:04:44 -0600

    removed bad example

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,7 +3,6 @@ extern crate alloc;
 use reactor::*;
 use alloc::boxed::Box;
 use alloc::rc::Rc;
-use core::cell::RefCell;
 
 #[test]
 #[ignore]
@@ -21,44 +20,6 @@ fn check_struct_sizes() {
     println!("sizeof<Box<dyn Behavior>> = {:?}", mem::size_of::<Box<dyn Behavior>>());
     println!("sizeof<Option<Effect>> = {:?}", mem::size_of::<Option<Effect>>());
     assert!(false);  // force failure!
-}
-
-struct CallCounter {
-    count: RefCell<usize>,
-}
-impl Behavior for CallCounter {
-    fn react(&self, _event: Event) -> Effect {
-        *self.count.borrow_mut() += 1;
-        println!("CallCounter.count = {}", *self.count.borrow());
-        Effect::new()
-    }
-}
-
-#[test]
-fn call_counter_behavior() {
-    struct Boot;
-    impl Behavior for Boot {
-        fn react(&self, _event: Event) -> Effect {
-            let mut effect = Effect::new();
-            println!("call_counter_behavior::Boot");
-
-            let counter = RefCell::new(0);
-            let beh = Box::new(CallCounter {
-                count: counter,
-            });
-            let actor = effect.create(beh);
-            effect.send(&actor, Message::Empty);
-
-            effect
-        }
-    }
-
-    let mut config = Config::new();
-    let count = config.boot(Box::new(Boot));
-    assert_eq!(1, count);
-
-    let count = config.dispatch(1);
-    assert_eq!(0, count);
 }
 
 #[test]
